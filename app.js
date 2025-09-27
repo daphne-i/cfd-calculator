8120ca2ae0fd4008fe2da81fe3d9983bbe9857e4
Daphne <daphne.issac@gmail.com>
2025-09-27 20:37:13 +0530

    PWA setup

--- app.js
+++ app.js
@@ -493,7 +493,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // --- PWA Service Worker Registration ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             })
